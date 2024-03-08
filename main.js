51551a75de920d340243499694ad71555bad5f74
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 08:59:09 -0800

    Q 34 of Ts/js & node js of 45 assignment.
    Q 34 of Ts/js & node js of 45 assignment.

---
+++ main.js
@@ -0,0 +1,10 @@
+var myPizza = ["Cheese pizza", "Pepperoni pizza", "Vegetarian pizza"];
+//print only names of pizza
+for (var i = 0; i < myPizza.length; i++) {
+    console.log(myPizza[i]);
+}
+//printing namesv and sentence/message
+for (var i = 0; i < myPizza.length; i++) {
+    console.log("I like to eat ".concat(myPizza[i]));
+}
+console.log("\n I really like to eat pizzas. Pizzza comes in a variety of flowers and toopings,allowing individuals to customize it to their liking");
